d80e60cb9c9ea3d8dc65e66567cc570f36ee78d8
chefache <stefan.m.koev@gmail.com>
2021-03-29 20:53:22 +0300

    Add Recipe and all related tables.

---
+++ Data/MyRecipes.Data.Models/Category.cs
@@ -0,0 +1,20 @@
+﻿namespace MyRecipes.Data.Models
+{
+    using System.Collections.Generic;
+
+    using MyRecipes.Data.Common.Models;
+
+    public class Category : BaseDeletableModel<int>
+    {
+        public Category()
+        {
+            this.Recipes = new HashSet<Recipe>();
+        }
+
+        public string Name { get; set; }
+
+        public int RecipeId { get; set; }
+
+        public virtual ICollection<Recipe> Recipes { get; set; }
+    }
+}
---
+++ Data/MyRecipes.Data.Models/Image.cs
@@ -0,0 +1,25 @@
+﻿namespace MyRecipes.Data.Models
+{
+    using System;
+
+    using MyRecipes.Data.Common.Models;
+
+    public class Image : BaseModel<string>
+    {
+        public Image()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+
+        public string AddedByUserId { get; set; }
+
+        public ApplicationUser AddedByUser { get; set; }
+
+        // The contents of the images are in the file sistem
+        public string Extension { get; set; }
+
+        public int RecipeId { get; set; }
+
+        public virtual Recipe Recipe { get; set; }
+    }
+}
